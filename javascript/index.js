68b8f40cd3f1ca7268b549652393116ee6416053
Jacob Angulo <jacobangulo@hotmail.com>
2019-01-27 23:29:56 -0800

    created user class and user post class in js.index. moved the recommended and ads boxes up

---
+++ javascript/index.js
@@ -0,0 +1,18 @@
+class User {
+    constructor(input) {
+        this.firstName = input.firstName;
+        this.lastName = input.lastName;
+        this.age = input.age;
+        this.birthday = input.birthday;
+    }
+}
+
+class UserPost extends User {
+    constructor(input) {
+        super(input);
+
+        post(input) {
+            return `${input}`;
+        }
+    }
+}
